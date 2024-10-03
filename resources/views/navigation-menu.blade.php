f5c42376781bef2e5eca45d66734fc1afb5e0dbd
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2024-10-03 23:52:09 +0700

    add role to register

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -11,15 +11,17 @@
                 <x-nav-link href="{{ route('books.index') }}" :active="request()->routeIs('books.index')">
                     {{ __('Books') }}
                 </x-nav-link>
-                <x-nav-link href="{{ route('admin.categories.index') }}" :active="request()->routeIs('admin.categories.index')">
-                    {{ __('Manage Category') }}
-                </x-nav-link>
-                <x-nav-link href="{{ route('admin.books.index') }}" :active="request()->routeIs('admin.books.index')">
-                    {{ __('Manage Books') }}
-                </x-nav-link>
-                <x-nav-link href="{{ route('admin.users.index') }}" :active="request()->routeIs('admin.users.index')">
-                    {{ __('Manage Users') }}
-                </x-nav-link>
+                @role('admin')
+                    <x-nav-link href="{{ route('admin.categories.index') }}" :active="request()->routeIs('admin.categories.index')">
+                        {{ __('Manage Category') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('admin.books.index') }}" :active="request()->routeIs('admin.books.index')">
+                        {{ __('Manage Books') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('admin.users.index') }}" :active="request()->routeIs('admin.users.index')">
+                        {{ __('Manage Users') }}
+                    </x-nav-link>
+                @endrole
             </span>
         </div>
         <div class="block pt-2 pb-3 space-x-5 space-y-1 md:hidden">
--- routes/web.php
+++ routes/web.php
@@ -9,23 +9,37 @@ use App\Http\Controllers\UserController;
 use App\Models\Chapter;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, 'index'])->name('index');
+Route::get('/', [IndexController::class, 'index'])
+    ->name('index');
 
 Route::middleware('auth')->group(function () {
     Route::prefix('admin')->name('admin.')->group(function () {
-        Route::resource('/users', UserController::class);
-        Route::resource('/books', BookController::class);
-        Route::resource('/categories', CategoryController::class);
-        Route::resource('/chapter', ChapterController::class);
-        Route::resource('/page', PageController::class);
+        Route::resource('/users', UserController::class)
+            ->middleware('admin');
+        Route::resource('/books', BookController::class)
+            ->middleware('admin');
+        Route::resource('/categories', CategoryController::class)
+            ->middleware('admin');
+        Route::resource('/chapter', ChapterController::class)
+            ->middleware('admin');
+        Route::resource('/page', PageController::class)
+            ->middleware('admin');
     });
 });
 
 // Letakkan rute dinamis setelah rute statis
-Route::get('/books', [IndexController::class, 'home'])->name('books.index');
-Route::get('/{book:slug}', [IndexController::class, 'indexShowBook'])->name('books.show');
-Route::get('/{book:slug}/{chapter:slug}', [IndexController::class, 'indexShowChapter'])->name('chapter.show');
-Route::get('/{book:slug}/{chapter:slug}/{page:slug}', [IndexController::class, 'indexShowPage'])->name('page.show');
+Route::get('/books', [IndexController::class, 'home'])
+    ->middleware('admin|editor|user')
+    ->name('books.index');
+Route::get('/{book:slug}', [IndexController::class, 'indexShowBook'])
+    ->middleware('admin|editor|user')
+    ->name('books.show');
+Route::get('/{book:slug}/{chapter:slug}', [IndexController::class, 'indexShowChapter'])
+    ->middleware('admin|editor|user')
+    ->name('chapter.show');
+Route::get('/{book:slug}/{chapter:slug}/{page:slug}', [IndexController::class, 'indexShowPage'])
+    ->middleware('admin|editor|user')
+    ->name('page.show');
 
 // Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
 // Route::post('/books', [BookController::class, 'store'])->name('books.store');
