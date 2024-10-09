81b2e841b3876247da1bcb5c27164201e01afbe2
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2024-10-09 20:06:05 +0700

    change edit photo

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -19,15 +19,48 @@
                     {{-- <x-input-error :messages="$errors->get('name')" class="mt-2" /> --}}
                 </div>
                 <div class="flex flex-grow gap-x-5">
-                    @if ($user->profile_photo_path)
-                        <img class="w-16 h-16 overflow-hidden bg-cover rounded-full"
-                            src="{{ Storage::url($user->profile_photo_path) }}" alt="">
-                    @endif
                     <span>
-                        <x-label for="photo" :value="__('photo')" />
+                        <div x-data="{ photoName: null, photoPreview: null }" class="col-span-6 sm:col-span-4">
+                            <!-- Profile Photo File Input -->
+                            <input id="profile_photo_path" class="hidden w-full mt-1" type="file"
+                                name="profile_photo_path" value="profile_photo_path" autofocus
+                                autocomplete="profile_photo_path" wire:model.live="profile_photo_path"
+                                x-ref="profile_photo_path"
+                                x-on:change="
+                                                photoName = $refs.profile_photo_path.files[0].name;
+                                                const reader = new FileReader();
+                                                reader.onload = (e) => {
+                                                    photoPreview = e.target.result;
+                                                };
+                                                reader.readAsDataURL($refs.profile_photo_path.files[0]);
+                                        " />
+
+                            <x-label for="photo" value="{{ __('Photo') }}" />
+
+                            <!-- Current Profile Photo -->
+                            <div class="mt-2" x-show="! photoPreview">
+                                <img src="{{ $user->profile_photo_url }}" alt="{{ $user->name }}"
+                                    class="object-cover w-20 h-20 rounded-full">
+                            </div>
+
+                            <!-- New Profile Photo Preview -->
+                            <div class="mt-2" x-show="photoPreview" style="display: none;">
+                                <span class="block w-20 h-20 bg-center bg-no-repeat bg-cover rounded-full"
+                                    x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
+                                </span>
+                            </div>
+
+                            <button class="px-3 py-2 mt-2 font-bold text-indigo-700 border-2 rounded-full"
+                                type="button" x-on:click.prevent="$refs.profile_photo_path.click()">
+                                {{ __('Select A New Photo') }}
+                            </button>
+
+                            <x-input-error for="photo" class="mt-2" />
+                        </div>
+                        {{-- <x-label for="photo" :value="__('photo')" />
                         <x-input id="profile_photo_path" class="block w-full mt-1" type="file"
                             name="profile_photo_path" value="profile_photo_path" autofocus
-                            autocomplete="profile_photo_path" />
+                            autocomplete="profile_photo_path" /> --}}
                         {{-- <x-input-error :messages="$errors->get('name')" class="mt-2" /> --}}
                     </span>
                 </div>
